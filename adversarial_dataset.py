3dcd06fd762bb0302d6259be117047fef3356058
rashikak295 <36055710+rashikak295@users.noreply.github.com>
2018-11-29 17:13:35 -0500

    iyuy

--- adversarial_dataset.py
+++ adversarial_dataset.py
@@ -4,3 +4,7 @@ import loader
 adversarial_samples_training_set = loader.generate_using_training_set(556)
 fname = 'adversarial_samples_training_set.pkl'
 pickle.dump(adversarial_samples_training_set, open(fname, 'wb'))
+
+# adversarial_samples_test_set = loader.generate(112)
+# fname = 'adversarial_samples_test_set.pkl'
+# pickle.dump(adversarial_samples_test_set, open(fname, 'wb'))
