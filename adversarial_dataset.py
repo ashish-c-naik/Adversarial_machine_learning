5f0892d8d135cc8a2215f1e8b804316cee52d716
rashikak295 <36055710+rashikak295@users.noreply.github.com>
2018-11-29 18:25:36 -0500

    dcfas

--- adversarial_dataset.py
+++ adversarial_dataset.py
@@ -1,10 +1,13 @@
 import pickle
 import loader
 
+# 50000 - Adversarial training samples
+# 10000 - Adversarial test samples
+
 adversarial_samples_training_set = loader.generate_using_training_set(556)
 fname = 'adversarial_samples_training_set.pkl'
 pickle.dump(adversarial_samples_training_set, open(fname, 'wb'))
 
-# adversarial_samples_test_set = loader.generate(112)
-# fname = 'adversarial_samples_test_set.pkl'
-# pickle.dump(adversarial_samples_test_set, open(fname, 'wb'))
+adversarial_samples_test_set = loader.generate(112)
+fname = 'adversarial_samples_test_set.pkl'
+pickle.dump(adversarial_samples_test_set, open(fname, 'wb'))
